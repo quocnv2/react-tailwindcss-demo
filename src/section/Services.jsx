cca7543389ed56d5c7f8d965a1a1a98337255d51
Quoc <quoc.nguyenvan2000@gmail.com>
2023-08-23 16:35:35 +0700

    done

---
+++ src/section/Services.jsx
@@ -0,0 +1,9 @@
+import {services} from "../constants/index.js";
+import ServiceCard from "../components/ServiceCard.jsx";
+
+const Servíces = () => {
+    return (<section className="max-container flex justify-center flex-wrap gap-9">
+            {services.map((service) => (<ServiceCard key={service.label}{...service}/>))}
+        </section>)
+}
+export default Servíces;
